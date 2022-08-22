2aa776f6471225ebc3fc90b5252f7987c7a95be5
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-08-22 14:01:07 +0200

    granted usage on sequence for generated key to user #patch

--- init/03_create_bamconfigbackend_priviliges.sql
+++ init/03_create_bamconfigbackend_priviliges.sql
@@ -1,5 +1,8 @@
 -- Allow user to access schema
 GRANT USAGE ON SCHEMA bamconfigbackend_user TO bamconfigbackend_user;
 
+-- Allow user to access generated keys
+GRANT USAGE ON SEQUENCE bamconfigbackend_user.statistic_statistic_id_seq TO bamconfigbackend_user;
+
 -- Allow user to execute CRUD operations
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA bamconfigbackend_user TO bamconfigbackend_user;
