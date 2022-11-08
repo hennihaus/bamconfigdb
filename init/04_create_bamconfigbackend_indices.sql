7a976280e07f7d3bb2ac4a746505ee8320101fc3
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-11-08 19:01:29 +0100

    added rule to protect erasure of teams #patch

--- init/04_create_bamconfigbackend_indices.sql
+++ init/04_create_bamconfigbackend_indices.sql
@@ -1,1 +1,2 @@
 CREATE UNIQUE INDEX ON bamconfigbackend_user.team(team_type) WHERE team_type = 'EXAMPLE';
+CREATE RULE protect_example_team AS ON DELETE TO bamconfigbackend_user.team WHERE team_type = 'EXAMPLE' DO INSTEAD NOTHING;
