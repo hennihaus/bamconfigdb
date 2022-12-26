966662f04201826524ed1b07e567d892e6d3d90b
janhendrik hausner <janhendrik.hausner@tchibo.de>
2022-12-26 07:30:22 +0100

    changed datetime from timestamp with timezone to date time without timezone #patch

--- init/02_create_bamconfigbackend_tables.sql
+++ init/02_create_bamconfigbackend_tables.sql
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.team
     team_username TEXT NOT NULL UNIQUE,
     team_password TEXT NOT NULL UNIQUE,
     team_jms_queue TEXT NOT NULL UNIQUE,
-    team_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    team_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    team_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    team_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT team_username_length CHECK (length(team_username) >= 6 AND length(team_username) <= 50),
     CONSTRAINT team_password_length CHECK (length(team_password) >= 10 AND length(team_password) <= 50),
     CONSTRAINT team_jms_queue_length CHECK (length(team_jms_queue) >= 6 AND length(team_jms_queue) <= 50)
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.student
     team_uuid UUID NOT NULL REFERENCES bamconfigbackend_user.team ON DELETE CASCADE ON UPDATE NO ACTION,
     student_firstname TEXT NOT NULL,
     student_lastname TEXT NOT NULL,
-    student_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    student_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    student_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    student_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT student_firstname_length CHECK (length(student_firstname) >= 2 AND length(student_firstname) <= 50),
     CONSTRAINT student_lastname_length CHECK (length(student_lastname) >= 2 AND length(student_lastname) <= 50)
 );
@@ -54,8 +54,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.contact
     contact_firstname TEXT NOT NULL,
     contact_lastname TEXT NOT NULL,
     contact_email TEXT NOT NULL,
-    contact_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    contact_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    contact_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    contact_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT contact_firstname_length CHECK (length(contact_firstname) >= 2 AND length(contact_firstname) <= 50),
     CONSTRAINT contact_lastname_length CHECK (length(contact_lastname) >= 2 AND length(contact_lastname) <= 50),
     CONSTRAINT contact_email_pattern CHECK (contact_email ~* '^[A-Za-z0-9._+%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$')
@@ -75,8 +75,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.task
     task_title TEXT NOT NULL UNIQUE,
     task_description TEXT NOT NULL,
     task_is_open_api_verbose BOOLEAN NOT NULL,
-    task_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    task_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    task_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    task_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT task_title_length CHECK (length(task_title) >= 6 AND length(task_title) <= 50),
     CONSTRAINT task_description_length CHECK (length(task_description) >= 0 AND length(task_description) <= 2000)
 );
@@ -113,8 +113,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.parameter
     parameter_type TEXT NOT NULL REFERENCES bamconfigbackend_user.parameter_type_enum ON DELETE CASCADE ON UPDATE NO ACTION,
     parameter_description TEXT NOT NULL,
     parameter_example TEXT NOT NULL,
-    parameter_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    parameter_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    parameter_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    parameter_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT parameter_name_length CHECK (length(parameter_name) >= 2 AND length(parameter_name) <= 50),
     CONSTRAINT parameter_description_length CHECK (length(parameter_description) >= 0 AND length(parameter_description) <= 100),
     CONSTRAINT parameter_example_length CHECK (length(parameter_example) >= 1 AND length(parameter_example) <= 50)
@@ -134,8 +134,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.response
     response_content_type TEXT NOT NULL,
     response_description  TEXT NOT NULL,
     response_example_json JSONB NOT NULL,
-    response_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    response_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    response_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    response_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT response_http_status_code_range CHECK (response_http_status_code >= -1 AND response_http_status_code <= 999),
     CONSTRAINT response_content_type_not_empty CHECK (length(response_content_type) >= 1),
     CONSTRAINT response_description_length CHECK (length(response_description) >= 1 AND length(response_description) <= 100),
@@ -158,8 +158,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.bank
     bank_thumbnail_url TEXT NOT NULL,
     bank_is_async BOOLEAN NOT NULL,
     bank_is_active BOOLEAN NOT NULL,
-    bank_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    bank_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    bank_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    bank_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT bank_name_length CHECK (length(bank_name) >= 6 AND length(bank_name) <= 50),
     CONSTRAINT bank_jms_queue_length CHECK (length(bank_jms_queue) >= 6 AND length(bank_jms_queue) <= 50),
     CONSTRAINT bank_thumbnail_url_pattern CHECK (bank_thumbnail_url ~* '(^$|^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?)')
@@ -171,8 +171,8 @@ CREATE TABLE IF NOT EXISTS bamconfigbackend_user.statistic
     bank_uuid UUID NOT NULL REFERENCES bamconfigbackend_user.bank ON DELETE CASCADE ON UPDATE NO ACTION,
     team_uuid UUID NOT NULL REFERENCES bamconfigbackend_user.team ON DELETE CASCADE ON UPDATE NO ACTION,
     statistic_requests_count BIGINT NOT NULL DEFAULT 0,
-    statistic_created_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
-    statistic_updated_timestamp_with_time_zone TIMESTAMPTZ NOT NULL DEFAULT now(),
+    statistic_created_timestamp TIMESTAMP NOT NULL DEFAULT now(),
+    statistic_updated_timestamp TIMESTAMP NOT NULL DEFAULT now(),
     CONSTRAINT bank_uuid_team_uuid_unique UNIQUE(bank_uuid, team_uuid),
     CONSTRAINT statistic_requests_count_positive_inclusive_zero CHECK (statistic_requests_count >= 0)
 );
